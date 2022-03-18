3ccd34ef6f4110129da633ca330679dbc6a7d062
choijaewoong <jwchoi0129@gmail.com>
2022-03-18 18:36:29 +0900

    build: Add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: ['prettier', 'react', '@typescript-eslint'],
+  extends: ['plugin:prettier/recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
