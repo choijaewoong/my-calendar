a1c9e2f6567de082f7b359bf6fda4d1e80cd8134
choijaewoong <jwchoi0129@gmail.com>
2022-07-23 16:44:33 +0900

    feat: Modify controls button

--- src/components/ControlsButton/ControlsButton.tsx
+++ src/components/ControlsButton/ControlsButton.tsx
@@ -1,7 +1,5 @@
 import React, { FC } from 'react';
 import styles from './ControlsButton.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 type Props = {
   handleControls: () => void;
@@ -11,7 +9,7 @@ const ControlsButton: FC<Props> = (props) => {
   const { handleControls } = props;
   return (
     <button className={styles.button_control} aria-label="controls open button" onClick={handleControls}>
-      <FontAwesomeIcon icon={solid('chevron-left')} />
+      <span className={styles.bar} />
     </button>
   );
 };
