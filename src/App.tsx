c687a6e97de4acd27187cc6d4276cff2ccdac746
choijaewoong <jwchoi0129@gmail.com>
2022-08-06 15:35:05 +0900

    build: Add mui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { RecoilRoot } from 'recoil';
 import Main from './pages/main';
 import './styles/style.scss';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#283593',
+      dark: '#1c2566',
+      light: '535da8',
+    },
+    secondary: {
+      main: '#ffa000',
+      dark: '#b27000',
+      light: '#ffb333',
+    },
+  },
+});
+
 function App() {
   return (
-    <RecoilRoot>
-      <Main />
-    </RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <RecoilRoot>
+        <Main />
+      </RecoilRoot>
+    </ThemeProvider>
   );
 }
 
